2907a1df0f1f00673ec72bec258b7a03c4ab9928
adambergere <acb257@cornell.edu>
2015-10-16 23:50:18 -0400

    added js

---
+++ website/assets/js/script.js
@@ -0,0 +1,18 @@
+
+function initalization () {
+	// $.get(, function{
+
+	// });
+	$('.post_title').text();
+	$('.post_body').text();
+}
+
+
+var json_practice = {"name":"adam"};
+
+
+
+
+$(document).ready(function() {   
+    initalization();
+});
