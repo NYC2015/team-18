e1cd994af2e73650bbb5be4833507be02dc234a6
amf272 <amf272@cornell.edu>
2015-10-17 09:37:39 -0400

    added fi

--- kcahealth/assets/js/expert.js
+++ kcahealth/assets/js/expert.js
@@ -12,16 +12,12 @@ $(document).ready(function() {
 // })
 
 
-$.getJSON( "/getPosts.json?lat="+lat+"&lon="+lon, function (response) {
+$.getJSON( "/getExperts.json", function (response) {
 		for (var i = 0; i < response.length; i++) {
 
 			expert = response[i][0];
 			email = response[i][1];
-			// <h2>TITLE OF POST</h2>
-			// 			<p>BODY OF POST</p>
-			// 			<h4>user</h4>
-			document.getElementById("forum").innerHTML+="<div class='postBox'><h2>"+expert+"</h2>"+"<h2>"+email+"</h2>"</div>";
-
-			// "<p>OP:"+OP+" body: "+body+"</p>";
-		};
+			document.getElementById("experts").innerHTML+="<div class='postBox'><h2>"+expert+"</h2><h2>"+email+"</h2></div>";
+		}
 	});
+})
