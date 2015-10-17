980665e48a740f693e999801ed39b3fe45c3bfb6
amf272 <amf272@cornell.edu>
2015-10-17 09:23:56 -0400

    added file

--- kcahealth/assets/js/expert.js
+++ kcahealth/assets/js/expert.js
@@ -3,9 +3,25 @@
 $(document).ready(function() {
   console.log("yep");
 
-$.getJSON( "/getStory.json", function (response) {
+// $.getJSON( "/getExperts.json", function (response) {
 
-  console.log(response);
-    document.getElementById("storyBody").innerHTML = "<div>"+response+"</div>";
-  });
-})
+
+//   console.log(response);
+//     document.getElementById("storyBody").innerHTML = "<div>"+response+"</div>";
+//   });
+// })
+
+
+$.getJSON( "/getPosts.json?lat="+lat+"&lon="+lon, function (response) {
+		for (var i = 0; i < response.length; i++) {
+
+			expert = response[i][0];
+			email = response[i][1];
+			// <h2>TITLE OF POST</h2>
+			// 			<p>BODY OF POST</p>
+			// 			<h4>user</h4>
+			document.getElementById("forum").innerHTML+="<div class='postBox'><h2>"+expert+"</h2>"+"<h2>"+email+"</h2>"</div>";
+
+			// "<p>OP:"+OP+" body: "+body+"</p>";
+		};
+	});
